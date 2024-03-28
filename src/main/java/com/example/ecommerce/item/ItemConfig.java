c52c8aa42b2b57add8e4101e49e9df395c7ca69c
Bilel Taboubi <80677927+bibolil@users.noreply.github.com>
2024-03-28 14:22:00 +0100

    updated items

--- src/main/java/com/example/ecommerce/item/ItemConfig.java
+++ src/main/java/com/example/ecommerce/item/ItemConfig.java
@@ -12,9 +12,9 @@ public class ItemConfig {
     CommandLineRunner commandLineRunner2(ItemRepository itemRepository)
     {
         return args->{
-            Item item1=new Item(1111,"new item",10.0,"picture",10);
-            Item item2=new Item(2222,"new item",10.0,"picture",10);
-            Item item3=new Item(3333,"new item",10.0,"picture",10);
+            Item item1=new Item(1111,"new item","brand new item",10.0,"picture",10,50,4);
+            Item item2=new Item(2222,"new item","brand new item",10.0,"picture",10,50,4);
+            Item item3=new Item(3333,"new item","brand new item",10.0,"picture",10,50,4);
         itemRepository.saveAll(List.of(item1,item2,item3));
         };
     }
